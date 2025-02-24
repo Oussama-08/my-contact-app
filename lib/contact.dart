0be28b19424cd67252de07e419cb12bb9e3d4bca
Oussama-08 <rahmounioussama36@gmail.com>
2025-02-24 16:44:06 +0100

    Statefull

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,23 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'widgets/socialmediaicon.dart';
 
-
-class Contact extends StatelessWidget {
+class Contact extends StatefulWidget {
   Contact({super.key});
 
-  Map <String,String> socialMedia = {
-    'facebook.png':'https://www.facebook.com/profile.php?id=100005313945397',
-    'Github.png'   :  'https://github.com',
-    'gmail.png'    :  'https://mail.google.com/mail/u/0/#sent',
-    'instagram.png':  'https://www.instagram.com/oussama.rhm/',
-    'whatsapp.png' :  'https://wa.me/+213793651350',
-    'tiktok.png'   :  'https://www.tiktok.com/@raouflivekick/video/7465736862636772613',
-    'In.png'       :  'https://fr.linkedin.com/',
+  @override
+  State<Contact> createState() => _ContactState();
+}
+
+class _ContactState extends State<Contact> {
+  String? socialmedia;
+  Uri? socialMedialink;
+
+  final Map<String, String> socialMediaMap = {
+    'facebook.png': 'https://www.facebook.com/profile.php?id=100005313945397',
+    'Github.png': 'https://github.com',
+    'gmail.png': 'https://mail.google.com/mail/u/0/#sent',
+    'instagram.png': 'https://www.instagram.com/oussama.rhm/',
+    'whatsapp.png': 'https://wa.me/+213793651350',
+    'tiktok.png':
+        'https://www.tiktok.com/@raouflivekick/video/7465736862636772613',
+    'In.png': 'https://fr.linkedin.com/',
   };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Icon(
+          Icons.home,
+          color: Colors.white,
+        ),
+        title: Text(
+          'Home Page',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: IconButton(
+                  onPressed: () {},
+                  icon: socialMedialink == null
+                      ? IconButton(
+                          onPressed: () {
+                            launchUrl(Uri.parse('tel:+213793651350'));
+                          },
+                          icon: Icon(Icons.phone_in_talk_outlined,
+                              color: Colors.white, size: 30))
+                      : Material(
+                          borderRadius: BorderRadius.circular(50),
+                          elevation: 4,
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          child: Image(
+                              image: AssetImage('assets/$socialmedia'))))),
+        ],
+      ),
       backgroundColor: Colors.blueGrey,
       body: SizedBox(
           width: double.infinity,
@@ -62,28 +104,41 @@ class Contact extends StatelessWidget {
                       width: 10,
                     ),
                     IconButton(
-                      icon: Icon(
-                        Icons.phone_in_talk_outlined,
-                        size: 30,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        launchUrl(Uri.parse('tel:+213793651350'));
-                      },
-                    )
+                        onPressed: () {
+                          launchUrl(Uri.parse('tel:+213793651350'));
+                        },
+                        icon: Icon(Icons.phone_in_talk_outlined,
+                            color: Colors.white, size: 30)),
                   ],
                 ),
                 SizedBox(
                   height: 15,
                 ),
                 GridView.builder(
-                  itemCount: socialMedia.length,
+                  itemCount: socialMediaMap.length,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 3),
                   itemBuilder: (context, index) {
-                    return SocialMediaIcon(
-                        socialMedia: socialMedia.keys.toList()[index],
-                        socialMedialink: socialMedia.values.toList()[index]);
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InkWell(
+                        child: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: AssetImage(
+                              'assets/${socialMediaMap.keys.toList()[index]}'),
+                          radius: 30,
+                        ),
+                        onTap: () {
+                          socialmedia = socialMediaMap.keys.toList()[index];
+                          socialMedialink =
+                              Uri.parse(socialMediaMap.values.toList()[index]);
+                          setState(() {
+                            launchUrl(socialMedialink!,
+                                mode: LaunchMode.externalApplication);
+                          });
+                        },
+                      ),
+                    );
                   },
                   physics: NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
@@ -94,5 +149,4 @@ class Contact extends StatelessWidget {
           )),
     );
   }
-
 }
